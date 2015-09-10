896b0c5c45bd3a732393c944d94652cde6297df1
Justin Joyce <jjoyce07@yahoo.com>
2015-09-10 16:09:30 -0500

    added variables to store scores and clear board after game

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 $( document ).ready(function() {
 
 	var playersTurn = "Player One";
-	 
+	var moveCount = 0;
+	var player1score = 0
+	var player2score = 0
+	var score = player1score + "vs" + player2score
 
 
 	$(".btn").click(function() {
 		if(playersTurn === "Player One"){
 	        $(this).text("x");
 	        $(this).css("color", "blue");
+	        moveCount++;
 	        playersTurn = "Player Two";
 	    }
 		else if(playersTurn === "Player Two"){
 			$(this).text("o");
 	        $(this).css("color", "red");
+	        moveCount++;
 	        playersTurn = "Player One";
 		}
 		
@@ -28,105 +33,120 @@ $( document ).ready(function() {
 		
 		if (btn1 == 'x' && btn2 == 'x' && btn3 == 'x'){ 
 			alert("Player1 Wins!");
+			player1score++;
+			console.log(player1score);
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn1 == 'o' && btn2 == 'o' && btn3 == 'o'){
 			alert("Player2 Wins!")
+			player2score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn4 == 'x' && btn5 == 'x' && btn6 == 'x'){
 			alert("Player1 Wins!")
+			player1score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn4 == 'o' && btn5 == 'o' && btn6 == 'o'){
 			alert("Player2 Wins!")
+			player2score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn7 == 'x' && btn8 == 'x' && btn9 == 'x'){
 			alert("Player1 Wins!")
+			player1score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn7 == 'o' && btn8 == 'o' && btn9 == 'o'){
 			alert("Player2 Wins!")
+			player2score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn1 == 'x' && btn4 == 'x' && btn7 == 'x'){
 			alert("Player1 Wins!")
+			player1score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn1 == 'o' && btn4 == 'o' && btn7 == 'o'){
 			alert("Player2 Wins!")
+			player2score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn2 == 'x' && btn5 == 'x' && btn8 == 'x'){
 			alert("Player1 Wins!")
+			player1score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn2 == 'o' && btn5 == 'o' && btn8 == 'o'){
 			alert("Player2 Wins!")
+			player2score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn3 == 'x' && btn6 == 'x' && btn9 == 'x'){
 			alert("Player1 Wins!")
+			player1score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn3 == 'o' && btn6 == 'o' && btn9 == 'o'){
 			alert("Player2 Wins!")
+			player2score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn1 == 'x' && btn5 == 'x' && btn9 == 'x'){
 			alert("Player1 Wins!")
+			player1score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn1 == 'o' && btn5 == 'o' && btn9 == 'o'){
 			alert("Player2 Wins!")
+			player2score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn3 == 'x' && btn5 == 'x' && btn7 == 'x'){
 			alert("Player1 Wins!")
+			player1score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
 		else if (btn3 == 'o' && btn5 == 'o' && btn7 == 'o'){
 			alert("Player2 Wins!")
+			player2score++;
+			$(".btn").text("");
+			moveCount = 0;
 		}
-		// else if ( $(".btn").text() == "x" || "o" ) {
-		// 	alert("meow")
-		// }
+		else if ( moveCount >= 9) {
+			alert("It's a Draw!")
+			moveCount = 0;
+		}
+		else{
+			return;
+		}
+
+
 
 		});
-//create a counter that will decide who won the game and store it in a variable
-//***Done***create if statements to decide who the winner is
-//need event handler
-		// var btn1 = $(".btn1").text();
-		// var btn2 = $(".btn2").text();
-		// var btn3 = $(".btn3").text();
-		// $(".btn").click(function(){
-		// console.log(btn1)
-		// if (btn1 == 'x' && btn2 == 'x' && btn3 == 'x'){ 
-		// 	alert("Player1 Wins!");
-		// }
-		// });
-		
-		
-	 
-
-
-		// check rows
-			// val0 = $(".btn1").value;
-			// val1 = $(".btn2").value;
-			// val2 = $(".btn3").value;
-			// if(val0 == 'x' && val1 == 'x' && val2 == 'x')
-			// {
-			// status.innerHTML = "X WINS!";
-			// return true;
-			// }
-			// else if(val0 == 'o' && val1 == 'o' && val2 == 'o')
-			// {
-			// status.innerHTML = "O WINS!";
-			// return true;
-			// }
-			// })
-
-		// $("#reset").click(function (){
-
-	 //    player1Name = $("#player1").val();
-	 //    player2Name = $("#player2").val();
-
-	 //    if(player1Name=="" || player2Name==""){
-	 //        alert("Please set player all the names.");
-	 //        return;
-	 //    }
-
-	 //    setTurn();
-
+//***DONE***creat a variable that stores wins & add 1 to each if else win
+//SCOREBOARD create a counter that will decide who won the game and store it in a variable
+//***DONE***when player wins clears board.
+//***DONE***create if statements to decide who the winner is
+//be able to pick o or x
 
 });
-	
+//why is reset button not working anymore???	
 	$("#reset").click(function(){
 		$(".btn").text("");
 	});
